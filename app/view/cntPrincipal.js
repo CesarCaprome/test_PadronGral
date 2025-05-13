a9963320c19ffc734525d60659bf9577193c290f
CesarCaprome <cesarcaprome@gmail.com>
2025-05-13 09:57:49 -0300

    Modificacion en cntCirMed

--- app/view/cntPrincipal.js
+++ app/view/cntPrincipal.js
@@ -17,7 +17,6 @@ Ext.define('App_Test_PadronGral.view.cntPrincipal', {
     extend: 'Ext.window.Window',
     alias: 'widget.cntprincipal',
 
-    mixins: '// ver si graba en VSC',
     requires: [
         'App_Test_PadronGral.view.cntPrincipalViewModel',
         'Ext.form.FieldContainer',
